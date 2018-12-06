1d21001a96a1aeaedc1ed57bd85678381ddd6d23
Samir Baraily <samir.baraily@gmail.com>
2018-12-05 19:21:51 -0700

    add testing suite and generate models

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,9 @@
+Module ControllerMacros
+   Def login_user
+        before(:each) do
+	     @request.env[“devise.mapping”] = Devise.mappings[:user]
+	     @user = User.create(email: ‘test@test.com’, password: ‘password’, first_name: ‘bob’, last_name: ‘boson’, age: 22)
+	     sign_in @user
+	    end
+	end
+end
